02a36c20f065b0d769c45ff0cee53a8677b6ee4e
ysban <covj12@gmail.com>
2022-12-26 14:55:43 +0900

    사전 자동 리로드 추가

--- mecab-loader/src/main/java/org/bitbucket/eunjeon/mecab_ko_mecab_loader/MeCabLoader.java
+++ mecab-loader/src/main/java/org/bitbucket/eunjeon/mecab_ko_mecab_loader/MeCabLoader.java
@@ -15,7 +15,9 @@
  ******************************************************************************/
 package org.bitbucket.eunjeon.mecab_ko_mecab_loader;
 
+import org.apache.logging.log4j.Logger;
 import org.chasen.mecab.Model;
+import org.elasticsearch.common.logging.Loggers;
 
 import java.security.AccessController;
 import java.security.PrivilegedAction;
@@ -24,6 +26,7 @@ import java.util.WeakHashMap;
 
 public final class MeCabLoader {
   private static Map<String, Model> models = new WeakHashMap<>();
+  private static Logger logger = Loggers.getLogger(MeCabLoader.class, "");
   static {
     AccessController.doPrivileged((PrivilegedAction<Void>) () -> {
       try {
@@ -42,12 +45,27 @@ public final class MeCabLoader {
   }
 
   public static synchronized Model getModel(String args) throws RuntimeException {
-    Model model = models.get(args);
-    if (model == null) {
-      model = new Model(args);
-      models.put(args, model);
+    Model currentModel = models.get(args);
+
+    if (currentModel == null) {
+      currentModel = new Model(args);
+      models.put(args, currentModel);
+    } else {
+      Model latestModel = new Model(args);
+      Long currentSize = currentModel.dictionary_info().getSize();
+      Long latestSize = latestModel.dictionary_info().getSize();
+
+      // 사전 크기가 변경되었다면 모델을 최신으로 교체한다
+      if(latestSize != 0){
+        if(!currentSize.equals(latestSize)){
+          logger.info("Compare Dictionary Size Current Dic. {} vs Latest Dic. {} ", currentSize, latestSize);
+          logger.info("Swap Dictionary ... ");
+          currentModel.swap(latestModel);
+          models.put(args, currentModel);
+        }
+      }
     }
-    return model;
+    return currentModel;
   }
 
   public static int getModelCount() {
